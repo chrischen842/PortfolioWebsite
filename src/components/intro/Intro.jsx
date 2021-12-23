4388537c41cb35fe40bcc065439093e5077a8344
Chris <cchen842@uwo.ca>
2021-12-23 17:47:30 -0500

    added changes to the topbar

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,8 +2,6 @@ import './intro.scss'
 
 export default function Intro() {
     return (
-        <div className='intro' id = 'intro'>
-            asdasd
-        </div>
+        <div className='intro' id = 'intro'></div>
     )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,28 @@
 import './topbar.scss'
+import {Phone, Mail} from '@material-ui/icons';
 
-export default function Topbar() {
+export default function Topbar({openMenu, setOpenMenu}) {
     return (
         <div className='topbar'>
             <div className='wrapper'>
                 <div className='left'>
                     <a href='#intro' className='logo'>Chris.</a>
+                    <div className="itemContainer">
+                        <Phone className='icon'/>
+                        <span>+1 (647)-822-8765</span>
+                    </div>
+                    <div className='itemContainer'>
+                        <Mail className='icon'/>
+                        <span>chrischen842@gmail.com</span>
+                    </div>
                 </div>
-                <div className='right'></div>
+                <div className='right'>
+                    <div className="hamburger">
+                        <span className='line1'></span>
+                        <span className='line2'></span>
+                        <span className='line3'></span>
+                    </div>
+                </div>
             </div>
         </div>
     )
