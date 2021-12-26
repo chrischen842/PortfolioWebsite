6938fff103272e464e0bd160a7e8e8ce37b735e9
Chris <cchen842@uwo.ca>
2021-12-26 00:09:58 -0500

    update to portfolio

--- src/portfolio/Portfolio.jsx
+++ src/portfolio/Portfolio.jsx
@@ -4,8 +4,17 @@ import React from 'react'
 
 export default function Portfolio() {
     return (
-        <div>
-            
+        <div className="portfolio" id = 'portfolio'>
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Discord Python Bot</li>
+                <li>Full Stack MERN</li>
+                <li>Connect-4 Web App</li>
+                <li>RPG Game</li>
+            </ul>
+            <div className="container">
+                <img src="" alt="" />
+            </div>
         </div>
     )
 }
