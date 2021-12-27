04c60f328833d27572c9d116d1cbc530ee4f3882
Chris <cchen842@uwo.ca>
2021-12-27 16:48:36 -0500

    update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -51,7 +51,7 @@ export default function Portfolio()
 
     return (
         <div className="portfolio" id = 'portfolio'>
-            <h1>Portfolio</h1>
+            <h1>Works Directory</h1>
             <ul>
                 {list.map(item => (
                     <PortfolioList 
