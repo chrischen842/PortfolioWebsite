d3a091f041329b9cba15e68a71bc708808037f30
Chris <cchen842@uwo.ca>
2021-12-28 19:27:57 -0500

    started aboutMe

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,9 +1,24 @@
 import './aboutMe.scss'
+import {GitHub} from '@material-ui/icons';
 
-export default function AboutMe() {
+export default function AboutMe() 
+{
     return (
         <div className='aboutMe' id = 'aboutMe'>
-            
+            <div className='wrapper'>
+                <h1>About Me</h1>
+                <div className='imageContainer'>
+                    <img src="static/fill.png" alt="" />
+                </div>
+                <div className='textContainer'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti explicabo, eum rerum molestiae repellat, deleniti delectus labore, esse ratione debitis id doloremque cupiditate veritatis placeat autem error ipsa deserunt earum.
+                    </p>
+                </div>  
+                <div className='icon'>
+                    <GitHub onClick={() => window.open('https://github.com/chrischen842', "_blank")}/>
+                </div>     
+            </div>
         </div>
     )
 }
